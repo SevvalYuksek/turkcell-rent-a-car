c72dbf4e0f619b69d10b9e38fd2328125bb6cc84
SevvalYuksek <74654417+SevvalYuksek@users.noreply.github.com>
2023-04-16 10:35:47 +0300

    Add files via upload

--- src/main/java/kodlama/io/rentacar/common/dto/CreateRentalPaymentRequest.java
+++ src/main/java/kodlama/io/rentacar/common/dto/CreateRentalPaymentRequest.java
@@ -1,28 +1,19 @@
-package kodlama.io.rentacar.entities;
+package kodlama.io.rentacar.common.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "payments")
-public class Payment {
-    @Id
-    @GeneratedValue
-    private int id;
+public class CreateRentalPaymentRequest {
     private String cardNumber;
     private String cardHolder;
     private int cardExpirationYear;
     private int cardExpirationMonth;
     private String cardCvv;
-    private double balance;
+    private double price;
 }
